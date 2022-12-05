44a7f86f358d978516b5b9c6f93fba0c5c4a2492
Andres <andrewking017@gmail.com>
2022-12-05 09:09:13 +0100

    mejorado el buscador

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -18,11 +18,16 @@ export class SearchBarComponent implements OnInit {
   @Input() listaLocaliades:any[]=[];
   @Output() searchValue = new EventEmitter<string>();
 
+  ubicacionABuscar:string= "";
+
   constructor(){}
   ngOnInit(): void {}
 
   getUbicacion(value: string){
-    this.searchValue.emit(value);
+    if (this.ubicacionABuscar!=""){
+      this.searchValue.emit(this.ubicacionABuscar);
+    }
+
   }
 
 
@@ -33,16 +38,47 @@ addSugerencia(event:any , sugerenciasDiv:any){
 
 
 
-  for (let item of this.listaLocaliades) {
-    if (entrada != "" && item.includes(entrada)) {
-      existe = true;
-      sugerencia = item;
+  // editamos la entrada de datos
+
+  let palabras = entrada.split(" ");
+  let primeraletra = "a";
+  let localidad:string[] = [];
+
+
+  for( let palabra of palabras.reverse()){
+      if (palabra.length > 2 && palabra!="LAS" && palabra!="LOS"){
+        localidad.push(palabra);
+        primeraletra = palabra[0];
+
       }
   }
+
+  // console.log('localidad a buscar : ' + localidad);
+  // console.log('primera letra : ' + primeraletra);
+
+  for (let item of this.listaLocaliades) {
+    if (item.includes(localidad[0])  && item[0] == primeraletra && item.includes(localidad[1]) && item.includes(localidad[2])) {
+      existe = true;
+      sugerencia = item;
+      }else if(item.includes(localidad[0])  && item[0] == primeraletra && item.includes(localidad[1])){
+        existe = true;
+        sugerencia = item;
+      }else if(item.includes(localidad[0])  && item[0] == primeraletra){
+        existe = true;
+        sugerencia = item;
+      // }else if(item.includes(localidad[0])) {
+      //   existe = true;
+      //   sugerencia = item;
+      }
+
+  }
+
+  console.log('sugerencia : ' + sugerencia);
 
   if (existe){
     sugerenciasDiv.innerHTML = sugerencia;
     sugerenciasDiv.style.display='block';
+    this.ubicacionABuscar = sugerencia;
   }else{
     sugerenciasDiv.style.display='none';
   }
