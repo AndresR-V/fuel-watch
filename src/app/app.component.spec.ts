225979773e1ada15dea61ae6d2f6660e13946b42
Andres <andrewking017@gmail.com>
2024-01-03 10:10:28 +0100

    updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,6 +32,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    // expect(compiled.querySelector('.content span')?.textContent).toContain('FuelWatch app is running!');
+
   });
 });
